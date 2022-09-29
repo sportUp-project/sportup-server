3437f6ce74ea32a23856fed13f046765b7896264
Joanna Zielinska <joannazielinskam@gmail.com>
2022-09-29 11:48:11 +0200

    auth on server side

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
-const allRoutes = require("./routes/index.routes");
+const allRoutes = require("./routes");
 app.use("/api", allRoutes);
+
+const authRouter = require("./routes/auth.routes");          
+app.use("/auth", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
---
+++ routes/auth.routes.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+
+const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
+
+const router = express.Router();
+const saltRounds = 10;
+
+router.post('/signup', (req,res,next) => {
+    const { email, password, name } = req.body;
+  
+    if (email === '' || password === '' || name === '') {
+      res.status(400).json({ message: "Provide email, password and name" });
+      return;
+    }
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: 'Provide a valid email address.' });
+      return;
+    }
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+      return;
+    }
+    User.findOne({ email })
+    .then((foundUser) => {
+      if (foundUser) {
+        res.status(400).json({ message: "User already exists." });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      // console.log({ email, password: hashedPassword, name })
+      return User.create({ email, password: hashedPassword, name });
+    })
+    .then((createdUser) => {
+      const { email, name, _id } = createdUser;
+      const user = { email, name, _id };
+
+      res.status(201).json({ user: user });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" })
+    });
+})
+
+router.post('/login', (req,res,next) => {
+    const { email, password } = req.body;
+  
+    if (email === '' || password === '') {
+      res.status(400).json({ message: "Provide email and password." });
+      return;
+    }
+    User.findOne({ email })
+    .then((foundUser) => {    
+      if (!foundUser) {
+        res.status(401).json({ message: "User not found." })
+        return;
+      }  
+      console.log(foundUser)
+      const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+      if (passwordCorrect) {      
+        const { _id, email, name, isAdmin } = foundUser;      
+        const payload = { _id, email, name, isAdmin };
+  
+        const authToken = jwt.sign( 
+          payload,
+          process.env.TOKEN_SECRET,
+          { algorithm: 'HS256', expiresIn: "6h" }
+        ); 
+          res.status(200).json({ authToken: authToken });
+      }
+      else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }  
+    })
+    .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+});
+
+router.get('/verify', isAuthenticated, (req,res,next) => {
+    res.status(200).json(req.payload);
+});
+
+module.exports = router;
